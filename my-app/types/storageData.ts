ef77294b0fbada30b621256a9ffdf82e9630135e
Nakagawadesu <nukitaro@outlook.com>
2024-06-17 07:20:24 -0300

    Add types for user profile, session token, and user preference

---
+++ my-app/types/storageData.ts
@@ -0,0 +1,15 @@
+export type userProfile = {
+  name: string;
+  email: string;
+  password: string;
+  birth: string;
+};
+
+export type sessionToken = {
+  token: string;
+};
+
+export type userPreference = {
+  theme: string;
+  fontSize: number;
+};
