8562f36a2d8e2c45db3785b9331523d2fb4f9024
nukitaro@outlook.com <matheus@Matheus.matheus>
2024-06-20 17:17:46 -0300

    ✨ - feat: Update HomeScreen component with new image and login link

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -1,39 +1,58 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { Link } from 'expo-router';
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { Link } from "expo-router";
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require('@/assets/images/NotColorfullPatterns.png')}
+          source={require("@/assets/images/NotColorfullPatterns.png")}
           resizeMode="cover"
         />
-      }>
+      }
+    >
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Bem Vindo ao Ecdysis!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="subtitle">Faça o Login para usufluir da nossa plataforma</ThemedText>
+        <ThemedText type="subtitle">
+          Faça o Login para usufluir da nossa plataforma
+        </ThemedText>
       </ThemedView>
       <ThemedView>
-      <Link replace href="/loginScreen">Login</Link>
-    </ThemedView>
+        <Link replace href="/loginScreen">
+          Login
+        </Link>
+      </ThemedView>
+      <ThemedView>
+        <Link replace href="/registerScreen">
+          Register
+        </Link>
+      </ThemedView>
+      <ThemedView>
+        <Link replace href="/homeScreen">
+          Home
+        </Link>
+      </ThemedView>
+      <ThemedView>
+        <Link replace href="/TakePicture">
+          TakePicture
+        </Link>
+      </ThemedView>
     </ParallaxScrollView>
-   
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -45,6 +64,6 @@ const styles = StyleSheet.create({
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
 });
