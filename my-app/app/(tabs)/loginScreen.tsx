031de52283d318f3bd2b846d7d8c9047c6f0fa85
Nakagawadesu <nukitaro@outlook.com>
2024-06-24 07:17:06 -0300

    Add login functionality and API URL constant

--- my-app/app/(tabs)/loginScreen.tsx
+++ my-app/app/(tabs)/loginScreen.tsx
@@ -1,9 +1,8 @@
-// app/LoginScreen.tsx
 import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import { Layout, Input, Button, Text } from "@ui-kitten/components";
 import { router } from "expo-router";
-
+import {REACT_APP_API_URL} from "@/types/EnvGambiarra";
 export default function LoginScreen() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -13,29 +12,42 @@ export default function LoginScreen() {
   const handleLogin = async () => {
     setLoading(true);
     setError("");
-    try {
-      const response = await fetch("http://192.168.1.55:3000/api/users/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username,
-          password,
-        }),
-        x,
-      });
-
-      if (!response.ok) {
-        throw new Error("Login failed");
+  
+    const MIN_WAIT_TIME = 2000; 
+    const apiUrl = `${REACT_APP_API_URL}/api/users/auth/login`;
+    const loginRequest = async () => {
+      try {
+        console.log(apiUrl)
+        const response = await fetch(apiUrl, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username,
+            password,
+          }),
+        });
+  
+        if (!response.ok) {
+          throw new Error("Login failed");
+        }
+        const data = await response.json();
+        router.replace("Home");
+      } catch (err) {
+        setError((err as Error).message);
+        throw err; // Re-throw the error to ensure loading state is reset
+      } finally {
+        setLoading(false); // Move setLoading(false) inside the finally block
       }
-      console.log(response);
-      const data = await response.json();
-      router.replace("Home");
-    } catch (err) {
-      setError((err as Error).message);
+    };
+  
+    const timer = new Promise<void>((resolve) => setTimeout(resolve, MIN_WAIT_TIME));
+  
+    try {
+      await Promise.all([loginRequest(), timer]);
     } finally {
-      setLoading(false);
+     setLoading(false);
     }
   };
 
@@ -58,7 +70,7 @@ export default function LoginScreen() {
         style={styles.input}
       />
       {error ? <Text status="danger">{error}</Text> : null}
-      <Button onPress={handleLogin} disabled={loading} style={styles.button}>
+      <Button onPress={()=>handleLogin()} disabled={loading} style={styles.button}>
         {loading ? "Logging in..." : "Login"}
       </Button>
     </Layout>
