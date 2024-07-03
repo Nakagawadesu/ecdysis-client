7dc33e8d285d69ac71cb42cbe6236ad5ba58859d
Nakagawadesu <nukitaro@outlook.com>
2024-07-03 08:16:14 -0300

    Update API URL and add token

--- my-app/types/EnvGambiarra.ts
+++ my-app/types/EnvGambiarra.ts
@@ -1,1 +1,3 @@
-export const REACT_APP_API_URL = "http://192.168.1.58:3000";
+export const REACT_APP_API_URL =
+  "https://32a1-2804-18-963-b3b4-6dd5-6090-82a9-99f7.ngrok-free.app";
+export const token = "2ibwZfu0I7vSTyM065ttcdhH1KI_853U5gssrnKpHTW1bCFta";
